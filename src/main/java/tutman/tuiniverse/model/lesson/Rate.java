62166f20b590be02d4b90451e3642efb8074c2af
Ruileirei <angruiwenray@gmail.com>
2025-10-31 01:23:26 +0800

    Fix edit.lesson rate bug

--- src/main/java/tutman/tuiniverse/model/lesson/Rate.java
+++ src/main/java/tutman/tuiniverse/model/lesson/Rate.java
@@ -32,9 +32,12 @@ public class Rate {
      * Checks if the given float is a valid rate.
      */
     public static boolean isValidRate(String str) {
-        float rate = Float.parseFloat(str);
-        return (rate >= 0);
-
+        try {
+            float rate = Float.parseFloat(str);
+            return (rate >= 0);
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     @Override
